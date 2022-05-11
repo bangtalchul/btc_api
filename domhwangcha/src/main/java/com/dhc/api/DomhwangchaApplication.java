f1997d691715872d330430e76db82a34e18c5851
soohyunKim <kimsoo5133@gmail.com>
2022-05-11 20:05:24 +0900

    preference setting

---
+++ domhwangcha/src/main/java/com/dhc/api/DomhwangchaApplication.java
@@ -0,0 +1,13 @@
+package com.dhc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomhwangchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomhwangchaApplication.class, args);
+	}
+
+}
---
+++ domhwangcha/src/test/java/com/dhc/api/DomhwangchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomhwangchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
