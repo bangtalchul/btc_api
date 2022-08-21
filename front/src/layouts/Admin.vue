89b9415887d324c080d75e145b823c95cee6cf33
문영화 <sosm8500@gmail.com>
2022-08-21 22:53:46 +0900

    Feat animate-slider

--- front/src/layouts/Admin.vue
+++ front/src/layouts/Admin.vue
@@ -2,10 +2,27 @@
   <div>
     <!-- <sidebar /> -->
     <index-navbar />
+      
       <section
-      class="header relative pt-16 items-center flex h-screen max-h-860-px"
+      class="header relative items-center flex pt-20 max-h-860-px"
     >
-    <div class="relative md:ml-64 bg-blueGray-100">
+    <div class="w-full bg-blueGray-100">
+           <div className="mainSkill bg-indigo-600 pb-16 relative overflow-hidden">
+                  <div className="flex flex-row mb-10 absolute animate-slider">
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                    <img className="w-100 h-full mx-10" src="@/assets/img/angular.jpg" alt="html logo" />
+                  </div>
+            </div>
       <admin-navbar />
       <header-stats />
       <!-- <div class="px-4 md:px-10 mx-auto w-full -m-24">
@@ -23,6 +40,12 @@ import HeaderStats from "@/components/Headers/HeaderStats.vue";
 // import FooterAdmin from "@/components/Footers/FooterAdmin.vue";
 import IndexNavbar from "@/components/Navbars/IndexNavbar.vue";
 export default {
+  data(){
+    return{
+      testimg: "@/img/angular.jpg",
+      testimg2: "@/assets/img/bootstrap.jpg",
+    }
+  },
   name: "admin-layout",
   components: {
     AdminNavbar,
